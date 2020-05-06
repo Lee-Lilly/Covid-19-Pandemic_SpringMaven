e3e0041e7d7ab2323f35a3a0daa5577006b5af8a
Lee <you@example.com>
2020-05-06 04:55:49 +0300

    Removed "Entity" from Timeline, no need Repos.

--- src/main/java/com/project/pandemic/domain/Timeline.java
+++ src/main/java/com/project/pandemic/domain/Timeline.java
@@ -3,14 +3,13 @@ package com.project.pandemic.domain;
 import java.util.Date;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity
+
 @Embeddable
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Timeline {
